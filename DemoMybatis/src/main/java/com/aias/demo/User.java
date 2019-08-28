e3ff68cf16c63b4a395fb3d3d289bcc048ff0ee2
aias00 <rokkki@163.com>
2019-08-28 13:06:35 +0800

    first commit

---
+++ DemoMybatis/src/main/java/com/aias/demo/User.java
@@ -0,0 +1,37 @@
+package com.aias.demo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
